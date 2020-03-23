505c0158d84f4d6fdfc9b2aef20f6d6d352ff2cd
Tricia Sykes <tlsykes98@gmail.com>
2020-03-23 12:26:00 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  withAuthenticator
+} from 'aws-amplify-react';
+import Amplify from 'aws-amplify';
+import config from './config'
+import { API, graphqlOperation } from 'aws-amplify'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+Amplify.configure(config)
+
+ 
+const query =  `query Query {
+  organization(id: "abc123") {
+    companies{
+      id name
+    }
+    id
+    name
+  }
+  
+}
+`
+class App extends Component {
+  state = { organizations: [ ] }
+  async componentDidMount(){
+   const data = await API.graphql(graphqlOperation(query)) 
+    this.setState({
+      organization: data.data.organization.name,
+      organizations: data.data.organization.companies
+    })
+ }
+
+  render() {
+
+  return ( 
+    <div className = "App" >
+    <header className = "App-header" >
+     <p className ="page-title"> Organizations: </p> 
+     <div className="org-name">
+     {
+       <p>{this.state.organization}</p>
+     }
+     </div>
+     <div className="list">
+       {
+       this.state.organizations.map((company, index ) => (
+         <p  key={index}>{company.id}:  {company.name}</p>
+       ))
+     }
+     </div>
+     </header>
+     </div>
   );
 }
+} 
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true } )
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    cognito: {
+        REGION: 'us-west-2',
+        USER_POOL_ID: 'us-west-2_hUkPGtQGz',
+        APP_CLIENT_ID: '6bds64b80seihk6ih44mqtsvqe',
+        // IDENTITY_POOL_ID: 'xxxx'
+
+    },
+    graphql: {
+        URL: 'https://p3ojwstxnfc6ngc3vqbczwmnfm.appsync-api.us-west-2.amazonaws.com/graphql',
+        REGION: 'us-west-2',
+        AUTHENTICATION_TYPE: 'AMAZON_COGNITO_USER_POOL',
+        
+
+    }
+}
